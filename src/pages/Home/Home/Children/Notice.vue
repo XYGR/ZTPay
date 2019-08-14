2b264256e23fe709299565cacfa39746629e8eeb
“YXGR” <1728335970@qq.com>
2019-08-14 22:06:11 +0800

    首页开始

---
+++ src/pages/Home/Home/Children/Notice.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="home-notice">
+        <h3 class="home-notice-title">
+             平台合规稳定运营<span>1</span>年<span>206</span>天
+        </h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "home-notice"
+    }
+</script>
+
+<style scoped lang="scss">
+    .home-notice{
+        padding: 0.4rem 0.533rem;
+        background-color: #ffffff;
+        &-title{
+            height: 0.8rem;
+            padding: 0.067rem 0;
+            background-color: #ccffff;
+        }
+    }
+</style>
---
+++ src/pages/Home/Home/Home.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="home" class="page">
+        <app-scroll>
+            <home-banner></home-banner>
+            <home-notice></home-notice>
+        </app-scroll>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    import AppScroll from "../../../components/AppScroll";
+    import Banner from "./Children/Banner";
+    import Notice from "./Children/Notice";
+    export default {
+        name: "Home",
+        components:{
+            [AppScroll.name]:AppScroll,
+            [Banner.name]:Banner,
+            [Notice.name]:Notice
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+
+</style>
