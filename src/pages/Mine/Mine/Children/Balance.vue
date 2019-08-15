326989d35b5a5eee33f0f70d5bdacda028f6e0fa
YXGR <1728335970@QQ.COM>
2019-08-16 01:51:04 +0800

    我的页面静态完成

--- src/pages/Mine/Mine/Children/Balance.vue
+++ src/pages/Mine/Mine/Children/Balance.vue
@@ -1,5 +1,24 @@
 <template>
-    <div class="mine-balance"></div>
+    <div class="mine-balance">
+        <div class="mine-balance-assets">
+            <h3 class="mine-balance-assets-title">总资产(元)</h3>
+            <p class="mine-balance-assets-money">
+                <span class="mine-balance-assets-money-before">0</span>
+                <span class="mine-balance-assets-money-after">.00</span>
+            </p>
+        </div>
+        <div class="mine-balance-profit">
+            <div class="mine-balance-profit-item interest">
+                <h4 class="mine-balance-profit-item-title">昨日利息(元)</h4>
+                <p class="mine-balance-profit-item-ele">0.00</p>
+            </div>
+            <div class="mine-balance-profit-item interest">
+                <h4 class="mine-balance-profit-item-title">昨日利息(元)</h4>
+                <p class="mine-balance-profit-item-ele">0.00</p>
+            </div>
+        </div>
+        <p class="mine-balance-tips">平均1分钟赚取不足0.01元...努力啊！</p>
+    </div>
 </template>
 
 <script>
@@ -13,8 +32,51 @@
         height: 5.12rem;
         border-radius: 0.267rem;
         background-color: #1F7DFD;
-        position: relative;
-        top: 0.693rem;
-        margin: 0 auto;
+        padding: 0 0.533rem;
+        transform: translateY(0.693rem);
+        &-assets{
+            padding-top: 0.267rem;
+            &-title{
+                font-size: 0.347rem;
+                line-height: 0.613rem;
+                color: #FFFFFF;
+                opacity: .5;
+            }
+            &-money{
+                font-family:ArialMT;
+                color: #FFFFFF;
+                font-weight: 400;
+                &-before{
+                    font-size: 0.72rem;
+                }
+                &-after{
+                    font-size: 0.507rem;
+                    font-weight: 400;
+                }
+            }
+        }
+        &-profit{
+            margin-top: 0.627rem;
+            display: flex;
+            &-item{
+                flex: 1;
+                color: #FFFFFF;
+                &-title{
+                    font-size: 0.347rem;
+                    line-height: 0.333rem;
+                    opacity: .5;
+                }
+                &-ele{
+                    font-size: 0.507rem;
+                    margin-top: 0.4rem;
+                }
+            }
+        }
+        &-tips{
+            font-size: 0.32rem;
+            color: #FFFFFF;
+            opacity: .5;
+            margin-top: 0.6rem;
+        }
     }
 </style>
--- src/pages/Mine/Mine/Mine.vue
+++ src/pages/Mine/Mine/Mine.vue
@@ -3,6 +3,8 @@
         <mine-header></mine-header>
         <app-scroll>
             <mine-balance></mine-balance>
+            <mime-link></mime-link>
+            <button class="mine-login-out">退出登录</button>
         </app-scroll>
         <router-view></router-view>
     </div>
@@ -12,13 +14,14 @@
     import AppScroll from "../../../components/AppScroll";
     import Header from "./Children/Header";
     import Balance from "./Children/Balance";
+    import Link from "./Children/Link";
     export default {
         name: "Mine",
         components:{
             [Header.name]:Header,
             [AppScroll.name]:AppScroll,
-            [Balance.name]:Balance
-
+            [Balance.name]:Balance,
+            [Link.name]:Link
         },
         mounted() {
 
@@ -35,6 +38,18 @@
             top: 1.6rem;
             bottom: 0;
             padding: 0 0.533rem;
+        }
+        .mine-login-out{
+            width: 100%;
+            height: 1.04rem;
+            font-size: 0.48rem;
+            line-height: 1.04rem;
+            color: #ffffff;
+            text-align: center;
+            margin-top: 2.133rem;
+            margin-bottom: 0.267rem;
+            background-color: #1F7DFD;
+            border-radius: 0.133rem;
         }
     }
 </style>
